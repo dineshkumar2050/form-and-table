6c450576939826f53f1e6a8f0bbfd6d99daaff75
dineshkumar2050 <dineshkumarjhangra@gmail.com>
2021-03-27 01:24:30 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [formData,setFormData] = useState({
+        username: '',
+        password: ''
+    })
+    const [tableData,setTableData] = useState([])
+    const [error,setError] = useState('');
+    const handleChange = e => {
+        e.preventDefault(); 
+        setError('')       
+        const { name,value } = e.target;
+        if(value && value.length > 0) setError('');
+        switch (name){
+            case 'username':
+                setFormData({...formData,username: value})
+                return;
+            case 'password':
+                setFormData({...formData,password: value})
+                return;
+            default:
+                return;
+        }
+    }
+    function checkAdmin(data){
+        if(data && data.length > 0) return true;
+    }
+    const handleSubmit = e => {
+        e.preventDefault();
+        let formInfo = {...formData}
+        const { username,password } = formInfo;
+        if(checkAdmin(username) && checkAdmin(password)) {
+          return setTableData([...tableData,{ username,password }])
+        }
+        setError('Please fill username & password');
+    }
+    return (
+        <div className="App">
+            <div className='form'>
+                <div className='username'>
+                    <label for='username'>Username</label>
+                    <input required type='text' id='username' placeholder={'Type username here'} name='username' onChange={handleChange} />
+                </div>
+                <div className='password'>
+                    <label for='password'>Password</label>
+                    <input required type='password' id='password' placeholder={'Type password here'} name='password' onChange={handleChange} />
+                </div>
+                <div className='submit button'>
+                    <button type='button' onClick={handleSubmit}>Submit</button>
+                </div>
+                <div className='error'>
+                    {
+                        error &&
+                        <span>{error}</span>
+                    }
+                </div>
+            </div>
+            <div className='table' style={{marginTop:'20px'}}>
+                <table style={{margin: '0 auto'}}>
+                    <tr>
+                        <th>S.No</th>
+                        <th>name</th>
+                        <th>Password</th>
+                    </tr>
+                    {
+                        tableData && tableData.length > 0 && tableData.map((data,dataIndex) => {
+                            return(
+                              <tr>
+                                  <td>{dataIndex + 1}</td>
+                                  {data.username && <td>{data.username}</td>}
+                                  {data.password && <td>{data.password}</td>}
+                              </tr>
+                            )
+                        })
+                    }                    
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default App;
